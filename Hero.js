23084ecee09576e9672f5e0245085dfff1ec2d45
Lraafe <rafael31alexandre@gmail.com>
2023-12-21 23:12:43 -0300

    Projeto Pronto

---
+++ Hero.js
@@ -0,0 +1,28 @@
+let hero = "Cleito Checha"
+let heroExp = 100000
+
+if(heroExp < 1000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Ferro")
+}else if(heroExp >= 1001 && heroExp <= 2000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Bronze")
+}else if(heroExp >= 2001 && heroExp <= 5000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Prata")
+}else if(heroExp >= 5001 && heroExp <= 7000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Ouro")
+}else if(heroExp >= 7001 && heroExp <= 8000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Platina")
+}else if(heroExp >= 8001 && heroExp <= 9000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Ascendente")
+}else if(heroExp >= 9001 && heroExp <= 10000) {
+    console.log("O Herói de nome " + hero + " está no nível " + heroExp + " e sua classificação é Imortal")
+}else if(heroExp >= 10001 && heroExp < 100000) {
+    console.log("O Herói de nome" + hero + " está no nível " + heroExp + " e sua classificação é Radiante")    
+}else if(heroExp >= 100000) {
+    let i = 0
+    while(i <= 69){
+        console.log("IRMÃO PARA, ISSO É UMA INTERVENÇÃO, PARE IMEDIATAMENTE DE JOGAR E VAI TOCAR EM GRAMA. Serio isso não é normal")
+        i++
+    }
+}
+
+
